77c2dabf43cb6bd83f86a3eed9c219e5cdc5da29
pnmougel <pnmougel@gmail.com>
2012-12-28 15:52:52 +1100

    Clean

--- target/scala-2.9.1/classes/public/javascripts/search.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  $('#page_container').pajinate({
-    nav_label_first: '&laquo',
-    nav_label_last: '&raquo',
-    items_per_page: 7,
-    abort_on_small_lists: true,
-    num_page_links_to_display: 3
-  });
-}).call(this);
